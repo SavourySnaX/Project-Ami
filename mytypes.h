14f8a314ccc165b49d75de96488510dae21296fc
SavourySnaX <savoury.snax@googlemail.com>
2009-07-13 13:56:43 +0100

    Fix up types for mac compilation

--- mytypes.h
+++ mytypes.h
@@ -2,14 +2,22 @@
 #ifndef __mytypes_h
 #define __mytypes_h
 
-
-typedef unsigned char   u_int8_t; 
+#ifndef _INT8_T
+#define _INT8_T 
 typedef char              int8_t;
+#endif
+typedef unsigned char   u_int8_t; 
                                
-typedef  unsigned short u_int16_t;
+#ifndef _INT16_T
+#define _INT16_T 
 typedef  short            int16_t;
+#endif
+typedef  unsigned short u_int16_t;
                                
-typedef  unsigned int   u_int32_t;
+#ifndef _INT32_T
+#define _INT32_T 
 typedef  int              int32_t;
+#endif
+typedef  unsigned int   u_int32_t;
 
 #endif//__mytypes_h
