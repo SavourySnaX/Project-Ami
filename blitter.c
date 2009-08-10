08527d0fc1f3af4145e1db2a35efca207d123201
SavourySnaX <savoury.snax@googlemail.com>
2009-08-10 20:26:50 +0100

    Reworked blitter code.  Added in line support.  Fixed up minterms, they are correct now.

--- blitter.c
+++ blitter.c
@@ -17,230 +17,426 @@
 #include "copper.h"
 #include "customchip.h"
 
-int bltStart=0;
 int bltZero=0;
+int bltSrcDone=0;
 
 u_int16_t	bltWidth;
 u_int16_t	bltHeight;
-u_int16_t	bltX,bltY;
+u_int16_t	bltSX,bltSY,bltDX,bltDY;
+u_int16_t	bltAOldDat,bltBOldDat;		// Stores last shifted out data
+int32_t		bltDelta;
+
+typedef enum
+{
+	BLT_Stopped = 0,
+	BLT_Read,		// Handles blitter src pre read, Blitter will read A&|B&|C data into BLTxDAT registers
+	BLT_Read2,		// Blitter will read A&|B&|C data into BLTxDAT registers
+	BLT_Line
+} BLT_Ops;
+
+BLT_Ops bltStart;
+
+typedef struct
+{
+	u_int8_t bits[16];
+} MTerms;
+
+MTerms	mTermA[65536];
+MTerms	mTermB[65536];
+MTerms	mTermC[65536];
 
 void BLT_InitialiseBlitter()
 {
-	bltStart=0;
-}
+	int a;
+	
+	bltStart=BLT_Stopped;
 
-u_int16_t BLT_GetNextBltSource(u_int16_t useMask,u_int8_t bltPtrStart,u_int8_t bltDataStart,u_int8_t bltModStart,int applyModulo)
-{
-	if (CST_GETWRDU(CST_BLTCON0,useMask))		// SRC is enabled
+	for (a=0;a<65536;a++)
 	{
-		u_int32_t	bltAAddress = CST_GETLNGU(bltPtrStart,0x0007FFFE);
-
-		CST_SETWRD(bltDataStart,MEM_getWord(bltAAddress),0xFFFF);
-		bltAAddress+=2;
-		
-		if (applyModulo)
+		for (int b=0;b<16;b++)
 		{
-			bltAAddress+=CST_GETWRDS(bltModStart,0xFFFE);
-		}
-		
-		CST_SETLNG(bltPtrStart,bltAAddress,0x0007FFFE);
-	}
-						
-	return CST_GETWRDU(bltDataStart,0xFFFF);
-}
-
-void BLT_SetDest(u_int16_t bltD,u_int8_t bltPtrStart,u_int8_t bltDataStart,u_int8_t bltModStart,int applyModulo)
-{
-	if (CST_GETWRDU(CST_BLTCON0,0x0100))		// DST is enabled
-	{
-		u_int32_t	bltAAddress = CST_GETLNGU(bltPtrStart,0x0007FFFE);
-
-		CST_SETWRD(bltDataStart,bltD,0xFFFF);
-		MEM_setWord(bltAAddress,bltD);
-		bltAAddress+=2;
-		
-		if (applyModulo)
-		{
-			bltAAddress+=CST_GETWRDS(bltModStart,0xFFFE);
-		}
-		
-		CST_SETLNG(bltPtrStart,bltAAddress,0x0007FFFE);
-	}
-}
-
-extern int startDebug;
-
-void BLT_Update()
-{
-	if (bltStart && CST_GETWRDU(CST_DMACONR,0x0240))
-	{
-		CST_ORWRD(CST_DMACONR,0x4000);
-		
-		if (cstMemory[0x43]&0x01)
-		{
-			int16_t tmp;
-			int16_t tmp2;
-			
-			printf("Blitter In Line Mode (unsupported)\n");
-			
-			switch ((cstMemory[0x43] & 0x1C)>>2)
+			if (a & (1<<b))
 			{
-			case 0:
-				printf("Blitter Octent : SSE\n");
-				break;
-			case 1:
-				printf("Blitter Octent : NNE\n");
-				break;
-			case 2:
-				printf("Blitter Octent : SSW\n");
-				break;
-			case 3:
-				printf("Blitter Octent : NNW\n");
-				break;
-			case 4:
-				printf("Blitter Octent : SEE\n");
-				break;
-			case 5:
-				printf("Blitter Octent : SWW\n");
-				break;
-			case 6:
-				printf("Blitter Octent : NEE\n");
-				break;
-			case 7:
-				printf("Blitter Octent : NWW\n");
-				break;
+				mTermA[a].bits[15-b]=4;
+				mTermB[a].bits[15-b]=2;
+				mTermC[a].bits[15-b]=1;
+			}
+		}
+	}
+	
+}
+
+void BLT_ReadNextBltSource(u_int16_t useMask,u_int8_t bltPtrStart,u_int8_t bltDataStart,u_int8_t bltModStart,int firstWrd,int lastWrd,
+						   u_int16_t fwdMask,u_int16_t lstMask,int shift,u_int16_t *old)
+{
+	if (CST_GETWRDU(CST_BLTCON0,useMask) && CST_GETWRDU(CST_DMACONR,0x0240)==0x0240)		// SRC is enabled
+	{
+		u_int32_t	bltAAddress = CST_GETLNGU(bltPtrStart,0x0007FFFE);
+		u_int16_t	bltADat = MEM_getWord(bltAAddress);
+
+		CST_SETWRD(bltDataStart,bltADat,0xFFFF);
+		bltAAddress+=bltDelta*2;
+		
+		if (lastWrd)
+		{
+			bltAAddress+=bltDelta*CST_GETWRDS(bltModStart,0xFFFE);
+		}
+		
+		CST_SETLNG(bltPtrStart,bltAAddress,0x0007FFFE);
+	}
+}
+
+
+// Should handle descending mode now, fill to do.
+u_int16_t BLT_CalculateD(u_int16_t bltA,u_int16_t bltB,u_int16_t bltC)
+{
+	u_int16_t	bltD=0;
+	int a;
+
+	for (a=0;a<16;a++)
+	{
+		if (CST_GETWRDU(CST_BLTCON0,1<<(mTermA[bltA].bits[a]+mTermB[bltB].bits[a]+mTermC[bltC].bits[a])))
+			bltD|=(1<<(15-a));
+	}
+
+	bltZero = bltZero && (bltD==0);
+	
+	return bltD;
+}
+
+void BLT_UpdateSrc()
+{
+	if (!bltSrcDone)
+	{
+		// The first/last word masks and shifters are applied before the register is held - so they are done in the read portion
+		BLT_ReadNextBltSource(0x0800,CST_BLTAPTH,CST_BLTADAT,CST_BLTAMOD,bltSX==0,bltSX==(bltWidth-1),
+							  CST_GETWRDU(CST_BLTAFWM,0xFFFF),CST_GETWRDU(CST_BLTALWM,0xFFFF),CST_GETWRDU(CST_BLTCON0,0xF000)>>12,&bltAOldDat);
+		BLT_ReadNextBltSource(0x0400,CST_BLTBPTH,CST_BLTBDAT,CST_BLTBMOD,bltSX==0,bltSX==(bltWidth-1),
+							  0xFFFF,0xFFFF,CST_GETWRDU(CST_BLTCON1,0xF000)>>12,&bltBOldDat);
+		BLT_ReadNextBltSource(0x0200,CST_BLTCPTH,CST_BLTCDAT,CST_BLTCMOD,bltSX==0,bltSX==(bltWidth-1),
+							  0xFFFF,0xFFFF,0,0);
+		
+	}	
+}
+
+void BLT_UpdateDst()
+{
+	if (CST_GETWRDU(CST_BLTCON0,0x0100) && CST_GETWRDU(CST_DMACONR,0x0240)==0x0240)		// DST is enabled
+	{
+		u_int32_t	bltDAddress = CST_GETLNGU(CST_BLTDPTH,0x0007FFFE);
+		
+		MEM_setWord(bltDAddress,CST_GETWRDU(CST_BLTDDAT,0xFFFF));
+		bltDAddress+=bltDelta * 2;
+		
+		if (bltDX==(bltWidth-1))
+		{
+			bltDAddress+=bltDelta * CST_GETWRDS(CST_BLTDMOD,0xFFFE);
+		}
+		
+		CST_SETLNG(CST_BLTDPTH,bltDAddress,0x0007FFFE);
+	}
+	
+	// Check End Condition
+	bltDX++;
+	if (bltDX == bltWidth)
+	{
+		bltDX = 0;
+		bltDY++;
+		if (bltDY>=bltHeight)
+		{
+			// blitter finished, clear BLTBUSY and set BLTComplete
+			bltStart=BLT_Stopped;
+			
+			CST_ORWRD(CST_INTREQR,0x0040);			// set interrupt blitter finished
+			CST_ANDWRD(CST_DMACONR,~0x4000);
+			
+			if (bltZero)
+			{
+				CST_ORWRD(CST_DMACONR,0x2000);		// BLT  all zeros
 			}
 			
-			tmp = cstMemory[0x62];
-			tmp<<=8;
-			tmp|= cstMemory[0x63];
-			
-			printf("dy = %d (%08X)\n",tmp/4,tmp);
-
-			tmp2 = cstMemory[0x64];
-			tmp2<<=8;
-			tmp2|= cstMemory[0x65];
-			
-			printf("dx = %d (%08X)\n",(tmp - tmp2)/4,tmp2);
-			
-			printf("line length = %d (%02X)\n",bltHeight,bltWidth);
-			
-//			startDebug=1;
-			
-								bltStart=0;
-								cstMemory[0x1F]|=0x40;			// set interrupt blitter finished
-								cstMemory[0x02]&=~(0x40);		// Clear busy
-			//SOFT_BREAK;
 		}
-		else
+	}
+}
+
+u_int16_t BLT_Mask(u_int16_t bltA)
+{
+		if (bltSX==0)
 		{
-			if (cstMemory[0x43]&0x02)
+			bltA&=CST_GETWRDU(CST_BLTAFWM,0xFFFF);
+		}
+		if (bltSX==(bltWidth-1))
+		{
+			bltA&=CST_GETWRDU(CST_BLTALWM,0xFFFF);
+		}
+		
+	return bltA;
+}
+
+u_int16_t BLT_Shift(u_int16_t bltA, u_int16_t shift, u_int16_t *old)
+{
+	u_int16_t bltOldDat,bltOldTmp;
+	
+		if (shift)
+		{
+			bltOldDat=*old;
+			if (CST_GETWRDU(CST_BLTCON1,0x0002) && !CST_GETWRDU(CST_BLTCON1,0x0001))			// descending mode shift left
 			{
-				printf("Blitter In Descending Mode (unsupported)\n");
-								bltStart=0;
-								cstMemory[0x1F]|=0x40;			// set interrupt blitter finished
-								cstMemory[0x02]&=~(0x40);		// Clear busy
-//				SOFT_BREAK;
+				bltOldTmp = (~((0x8000 >> (shift-1))-1));
+				bltOldTmp&=bltA;
+				bltOldTmp>>=(16-shift);
+				*old = bltOldTmp;
+				bltA<<=shift;
+				bltA|=bltOldDat;
 			}
 			else
 			{
-				if (cstMemory[0x43]&0x18)
+				bltOldTmp = (((0x0001 << shift)-1));
+				bltOldTmp&=bltA;
+				bltOldTmp<<=(16-shift);
+				*old = bltOldTmp;
+				bltA>>=shift;
+				bltA|=bltOldDat;
+			}
+		}
+	return bltA;
+}
+
+void BLT_UpdateClc()
+{
+	u_int16_t	bltA,bltB,bltC,bltD;
+	
+	bltA=CST_GETWRDU(CST_BLTADAT,0xFFFF);
+	bltB=CST_GETWRDU(CST_BLTBDAT,0xFFFF);
+
+//	if (!CST_GETWRDU(CST_BLTCON1,0x0001))
+	{
+		bltA=BLT_Mask(bltA);
+		
+		bltA=BLT_Shift(bltA,CST_GETWRDU(CST_BLTCON0,0xF000)>>12,&bltAOldDat);	
+
+		bltB=BLT_Shift(bltB,CST_GETWRDU(CST_BLTCON1,0xF000)>>12,&bltBOldDat);
+	}
+	
+	bltC=CST_GETWRDU(CST_BLTCDAT,0xFFFF);
+
+	if (!CST_GETWRDU(CST_BLTCON1,0x0001))
+	{
+		// Check End Src Read Condition
+		bltSX++;
+		if (bltSX == bltWidth)
+		{
+			bltSX = 0;
+			bltAOldDat=0;
+			bltBOldDat=0;
+			bltSY++;
+			if (bltSY>=bltHeight)
+			{
+				bltSrcDone=1;
+			}
+		}
+	}
+
+	bltD=BLT_CalculateD(bltA,bltB,bltC);
+	
+	CST_SETWRD(CST_BLTDDAT,bltD,0xFFFF);
+}
+
+// Modified BLT_Update to a simple state machine. Basically to cope with double src read before write as stated in HRM
+
+void BLT_Update()
+{
+	u_int32_t addr;
+	
+	switch (bltStart)
+	{
+		case BLT_Stopped:
+			break;
+		case BLT_Read:
+
+			CST_ORWRD(CST_DMACONR,0x4000);
+
+			BLT_UpdateSrc();
+			
+			BLT_UpdateClc();
+
+//			BLT_UpdateDst();				// swap this and next line comment over if trying for Preread
+			
+			bltStart=BLT_Read2;
+			break;
+			
+		case BLT_Read2:						// Doesn't seem to work quite right yet
+
+			CST_ORWRD(CST_DMACONR,0x4000);
+
+			BLT_UpdateSrc();
+
+			BLT_UpdateDst();
+			
+			BLT_UpdateClc();
+			
+			break;
+			
+		case BLT_Line:
+		
+			CST_ORWRD(CST_DMACONR,0x4000);
+
+			if (CST_GETWRDU(CST_BLTCON0,0x0200) && CST_GETWRDU(CST_DMACONR,0x0240)==0x0240)		// SRC is enabled
+			{
+				CST_SETWRD(CST_BLTCDAT,MEM_getWord(CST_GETLNGU(CST_BLTCPTH,0X0007FFFE)),0xFFFF);
+			}
+
+			BLT_UpdateClc();
+			
+			if (!CST_GETWRDU(CST_BLTCON1,0x0040))
+			{
+				if (CST_GETWRDU(CST_BLTCON0,0x0800))
 				{
-					printf("Blitter In Fill Mode (unsupported)\n");
-					SOFT_BREAK;
+					addr = CST_GETLNGU(CST_BLTAPTH,0x0007FFFE);
+					addr+=CST_GETWRDS(CST_BLTAMOD,0xFFFE);
+					CST_SETLNG(CST_BLTAPTH,addr,0x0007FFFE);
+				}
+				if (CST_GETWRDU(CST_BLTCON1,0x0010))
+				{
+					if (CST_GETWRDU(CST_BLTCON1,0x0008))
+					{
+						//decy
+						addr = CST_GETLNGU(CST_BLTCPTH,0x0007FFFE);
+						addr-=CST_GETWRDS(CST_BLTCMOD,0xFFFE);
+						CST_SETLNG(CST_BLTCPTH,addr,0x0007FFFE);
+					}
+					else
+					{
+						//incy
+						addr = CST_GETLNGU(CST_BLTCPTH,0x0007FFFE);
+						addr+=CST_GETWRDS(CST_BLTCMOD,0xFFFE);
+						CST_SETLNG(CST_BLTCPTH,addr,0x0007FFFE);
+					}
 				}
 				else
 				{
-/*					if ( (cstMemory[0x42]&0xF0) || (cstMemory[0x40]&0xF0) )
+					if (CST_GETWRDU(CST_BLTCON1,0x0008))
 					{
-						printf("Blitter Using Shift (unsupported)\n");
-								bltStart=0;
-								cstMemory[0x1F]|=0x40;			// set interrupt blitter finished
-								cstMemory[0x02]&=~(0x40);		// Clear busy
-//						SOFT_BREAK;
+						//decx
+						u_int16_t oldShift = CST_GETWRDU(CST_BLTCON0,0xF000)>>12;
+						oldShift--;
+						CST_ANDWRD(CST_BLTCON0,0x0FFF);
+						CST_ORWRD(CST_BLTCON0,(oldShift&0xF)<<12);
+						if (oldShift==0xFFFF)
+						{
+							addr = CST_GETLNGU(CST_BLTCPTH,0x0007FFFE);
+							addr-=2;
+							CST_SETLNG(CST_BLTCPTH,addr,0x0007FFFE);
+						}
 					}
-					else*/
+					else
 					{
-						u_int16_t	bltAMask=0xFFFF;
-						u_int16_t	bltA,bltB,bltC,bltD;
-						u_int16_t	bltMin0=0,bltMin1=0,bltMin2=0,bltMin3=0,bltMin4=0,bltMin5=0,bltMin6=0,bltMin7=0;
-						
-						if (bltX==0)
+						//incx
+						u_int16_t oldShift = CST_GETWRDU(CST_BLTCON0,0xF000)>>12;
+						oldShift++;
+						CST_ANDWRD(CST_BLTCON0,0x0FFF);
+						CST_ORWRD(CST_BLTCON0,(oldShift&0xF)<<12);
+						if (oldShift==0x0010)
 						{
-							bltAMask&=(((u_int16_t)cstMemory[0x44])<<8) | cstMemory[0x45];
-						}
-						if (bltX==(bltWidth-1))
-						{
-							bltAMask&=(((u_int16_t)cstMemory[0x46])<<8) | cstMemory[0x47];
-						}
-						
-						bltA = BLT_GetNextBltSource(0x0800,0x50,0x74,0x64,bltX==(bltWidth-1)) & bltAMask;
-						bltB = BLT_GetNextBltSource(0x0400,0x4C,0x72,0x62,bltX==(bltWidth-1));
-						bltC = BLT_GetNextBltSource(0x0200,0x48,0x70,0x60,bltX==(bltWidth-1));
-						
-						if (cstMemory[0x41]&0x80)
-						{
-							bltMin7 = (~bltA) & (~bltB) & (~bltC);
-						}
-						if (cstMemory[0x41]&0x40)
-						{
-							bltMin6 = (bltA) & (bltB) & (~bltC);
-						}
-						if (cstMemory[0x41]&0x20)
-						{
-							bltMin5 = (bltA) & (~bltB) & (bltC);
-						}
-						if (cstMemory[0x41]&0x10)
-						{
-							bltMin4 = (bltA) & (~bltB) & (~bltC);
-						}
-						if (cstMemory[0x41]&0x08)
-						{
-							bltMin3 = (~bltA) & (bltB) & (bltC);
-						}
-						if (cstMemory[0x41]&0x04)
-						{
-							bltMin2 = (~bltA) & (bltB) & (~bltC);
-						}
-						if (cstMemory[0x41]&0x02)
-						{
-							bltMin1 = (~bltA) & (~bltB) & (bltC);
-						}
-						if (cstMemory[0x41]&0x01)
-						{
-							bltMin0 = (bltA) & (bltB) & (bltC);
-						}
-						
-						bltD = bltMin0 | bltMin1 | bltMin2 | bltMin3 | bltMin4 | bltMin5 | bltMin6 | bltMin7;
-						
-						bltZero = bltZero && (bltD==0);
-						
-						BLT_SetDest(bltD,0x54,0x00,0x66,bltX==(bltWidth-1));
-						
-						if (bltX == (bltWidth-1))
-						{
-							bltX = 0;
-							bltY++;
-							if (bltY>=bltHeight)
-							{
-								// blitter finished, clear BLTBUSY and set BLTComplete
-								bltStart=0;
-								cstMemory[0x1F]|=0x40;			// set interrupt blitter finished
-								cstMemory[0x02]&=~(0x40);		// Clear busy
-								if (bltZero)
-								{
-									cstMemory[0x02]|=0x20;		// BLT  all zeros
-								}
-							}
-						}
-						else
-						{
-							bltX++;
+							addr = CST_GETLNGU(CST_BLTCPTH,0x0007FFFE);
+							addr+=2;
+							CST_SETLNG(CST_BLTCPTH,addr,0x0007FFFE);
 						}
 					}
 				}
 			}
-		}
+			else
+			{
+				if (CST_GETWRDU(CST_BLTCON0,0x0800))
+				{
+					addr = CST_GETLNGU(CST_BLTAPTH,0x0007FFFE);
+					addr+=CST_GETWRDS(CST_BLTBMOD,0xFFFE);
+					CST_SETLNG(CST_BLTAPTH,addr,0x0007FFFE);
+				}
+			
+			}
+			if (CST_GETWRDU(CST_BLTCON1,0x0010))
+			{
+				if (CST_GETWRDU(CST_BLTCON1,0x0004))
+				{
+					//decx
+					u_int16_t oldShift = CST_GETWRDU(CST_BLTCON0,0xF000)>>12;
+					oldShift--;
+					CST_ANDWRD(CST_BLTCON0,0x0FFF);
+					CST_ORWRD(CST_BLTCON0,(oldShift&0xF)<<12);
+					if (oldShift==0xFFFF)
+					{
+						addr = CST_GETLNGU(CST_BLTCPTH,0x0007FFFE);
+						addr-=2;
+						CST_SETLNG(CST_BLTCPTH,addr,0x0007FFFE);
+					}
+				}
+				else
+				{
+					//incx
+					u_int16_t oldShift = CST_GETWRDU(CST_BLTCON0,0xF000)>>12;
+					oldShift++;
+					CST_ANDWRD(CST_BLTCON0,0x0FFF);
+					CST_ORWRD(CST_BLTCON0,(oldShift&0xF)<<12);
+					if (oldShift==0x0010)
+					{
+						addr = CST_GETLNGU(CST_BLTCPTH,0x0007FFFE);
+						addr+=2;
+						CST_SETLNG(CST_BLTCPTH,addr,0x0007FFFE);
+					}
+				}
+			}
+			else
+			{
+				if (CST_GETWRDU(CST_BLTCON1,0x0004))
+				{
+					//decy
+					addr = CST_GETLNGU(CST_BLTCPTH,0x0007FFFE);
+					addr-=CST_GETWRDS(CST_BLTCMOD,0xFFFE);
+					CST_SETLNG(CST_BLTCPTH,addr,0x0007FFFE);
+				}
+				else
+				{
+					//incy
+					addr = CST_GETLNGU(CST_BLTCPTH,0x0007FFFE);
+					addr+=CST_GETWRDS(CST_BLTCMOD,0xFFFE);
+					CST_SETLNG(CST_BLTCPTH,addr,0x0007FFFE);
+				}
+			}
+						
+			if (CST_GETLNGU(CST_BLTAPTH,0x00008000))
+			{
+				CST_ORWRD(CST_BLTCON1,0x0040);
+			}
+			else
+			{
+				CST_ANDWRD(CST_BLTCON1,~0x0040);
+			}
+				
+			if (CST_GETWRDU(CST_BLTCON0,0x0100) && CST_GETWRDU(CST_DMACONR,0x0240)==0x0240)		// DST is enabled
+			{
+				MEM_setWord(CST_GETLNGU(CST_BLTDPTH,0x0007FFFE), CST_GETWRDU(CST_BLTDDAT,0xFFFF));
+			}
+
+			CST_SETLNG(CST_BLTDPTH,CST_GETLNGU(CST_BLTCPTH,0x0007FFFE),0x0007FFFE);
+
+			bltHeight--;
+			if (bltHeight==0)
+			{
+			// blitter finished, clear BLTBUSY and set BLTComplete
+			bltStart=BLT_Stopped;
+			
+			CST_ORWRD(CST_INTREQR,0x0040);			// set interrupt blitter finished
+			CST_ANDWRD(CST_DMACONR,~0x4000);
+			
+			if (bltZero)
+			{
+				CST_ORWRD(CST_DMACONR,0x2000);		// BLT  all zeros
+			}
+				
+			}
+
+			break;
 	}
 }
 
@@ -248,24 +444,72 @@ extern int startDebug;
 
 void BLT_StartBlit()
 {
-//		startDebug=1;
+	//startDebug=1;
+	
+		bltDX=0;
+		bltDY=0;
+		bltSX=0;
+		bltSY=0;
+		bltAOldDat=0;
+		bltBOldDat=0;
 		
-		bltX=0;
-		bltY=0;
+//		if (!CST_GETWRDU(CST_BLTCON1,0x0001))
+//			return;
+		
+//		lCount++;
+//		if (lCount<151 || lCount>151)
+//			return;
+//		printf("lCount : %d\n",lCount);
+
+		if (CST_GETWRDU(CST_BLTCON1,0x0001))
+		{
+			if (CST_GETWRDU(CST_BLTCON1,0x0002))
+			{
+				printf("Warning.. single dot mode not supported\n");
+				return;
+			}
+			
+			if (CST_GETWRDU(CST_BLTBDAT,0xFFFF)!=0xFFFF)
+			{
+				printf("Warning.. textured line not supported\n");
+				return;
+			}
+		}
+		else
+		{
+			if (CST_GETWRDU(CST_BLTCON1,0x0018))
+			{
+				printf("Warning.. blitter fill unsupported\n");
+				return;
+			}
+			if (CST_GETWRDU(CST_BLTCON1,0x0002))
+			{
+				bltDelta=-1;
+			}
+			else
+			{
+				bltDelta=1;
+			}
+  		}
+		
 		bltWidth=CST_GETWRDU(CST_BLTSIZE,0x003F);
 		if (!bltWidth)
 			bltWidth=64;
 		bltHeight=CST_GETWRDU(CST_BLTSIZE,0xFFC0)>>6;
 		if (!bltHeight)
 			bltHeight=1024;
+			
 		bltZero=1;
-		bltStart=1;
-		
+		bltSrcDone=0;
+
+		if (!CST_GETWRDU(CST_BLTCON1,0x0001))
+		{
+			bltStart=BLT_Read;
 		printf("[WRN] Blitter Is Being Used\n");
 
 		printf("Blitter Registers : BLTAFWM : %04X\n",CST_GETWRDU(CST_BLTAFWM,0xFFFF));
 		printf("Blitter Registers : BLTALWM : %04X\n",CST_GETWRDU(CST_BLTALWM,0xFFFF));
-		printf("Blitter Registers : BLTCON0 : %04X\n",CST_GETWRDU(CST_BLTCON0,0xFFFF));
+/*		printf("Blitter Registers : BLTCON0 : %04X\n",CST_GETWRDU(CST_BLTCON0,0xFFFF));
 		printf("Blitter Registers : BLTCON1 : %04X\n",CST_GETWRDU(CST_BLTCON1,0xFFFF));
 		printf("Blitter Registers : BLTSIZE : %04X\n",CST_GETWRDU(CST_BLTSIZE,0xFFFF));
 		printf("Blitter Registers : BLTADAT : %04X\n",CST_GETWRDU(CST_BLTADAT,0xFFFF));
@@ -277,8 +521,14 @@ void BLT_StartBlit()
 		printf("Blitter Registers : BLTCMOD : %04X\n",CST_GETWRDU(CST_BLTCMOD,0xFFFF));
 		printf("Blitter Registers : BLTDMOD : %04X\n",CST_GETWRDU(CST_BLTDMOD,0xFFFF));
 		printf("Blitter Registers : BLTAPTH : %08X\n",CST_GETLNGU(CST_BLTAPTH,0xFFFFFFFF));
-		printf("Blitter Registers : BLTBPTH : %08X\n",CST_GETLNGU(CST_BLTAPTH,0xFFFFFFFF));
-		printf("Blitter Registers : BLTCPTH : %08X\n",CST_GETLNGU(CST_BLTAPTH,0xFFFFFFFF));
-		printf("Blitter Registers : BLTDPTH : %08X\n",CST_GETLNGU(CST_BLTAPTH,0xFFFFFFFF));
-
+		printf("Blitter Registers : BLTBPTH : %08X\n",CST_GETLNGU(CST_BLTBPTH,0xFFFFFFFF));
+		printf("Blitter Registers : BLTCPTH : %08X\n",CST_GETLNGU(CST_BLTCPTH,0xFFFFFFFF));
+		printf("Blitter Registers : BLTDPTH : %08X\n",CST_GETLNGU(CST_BLTDPTH,0xFFFFFFFF));
+*/
+		}
+		else
+			bltStart=BLT_Line;
+	
+		
 }
+
